720e0ae1141cc3a3d9e3c8718d801ca95dc66479
L7R-a <gedylarosa@gmail.com>
2025-03-07 14:08:33 -0500

    22nd commit

--- src/test/java/com/sg/floormastery/dao/ProductsDAOFileImplTest.java
+++ src/test/java/com/sg/floormastery/dao/ProductsDAOFileImplTest.java
@@ -15,49 +15,63 @@ class ProductsDAOFileImplTest {
 
     @BeforeEach
     void setUp() throws Exception {
-        // Assuming the DAO takes a file path for initialization
         testDao = new ProductsDAOFileImpl();
     }
 
     @Test
-    void getAllProducts() {
+    void testGetAllProducts() {
         List<Product> products = testDao.getAllProducts();
         assertNotNull(products, "Product list should not be null");
         assertFalse(products.isEmpty(), "Product list should not be empty");
     }
 
     @Test
-    void getAllProductTypes() {
+    void testGetAllProductTypes() {
         Set<String> productTypes = testDao.getAllProductTypes();
         assertNotNull(productTypes, "Product types set should not be null");
         assertFalse(productTypes.isEmpty(), "Product types set should not be empty");
         assertEquals(4, productTypes.size(), "There should be 4 types");
         assertTrue(productTypes.contains("Wood"),
-                "Product types should wood as a type");
+                "Product types should have wood as a type");
         assertTrue(productTypes.contains("Laminate"),
-                "Product types should laminate as a type");
+                "Product types should have laminate as a type");
         assertTrue(productTypes.contains("Carpet"),
-                "Product types should carpet as a type");
+                "Product types should have carpet as a type");
         assertTrue(productTypes.contains("Tile"),
-                "Product types should tile as a type");
+                "Product types should have tile as a type");
     }
 
     @Test
-    void getProduct() {
+    void testGetProduct() {
         Product product = testDao.getProduct("Wood");
         assertNotNull(product, "Product should not be null");
-        assertEquals("Wood", product.getProductType(), "Product type should match requested type");
+        assertEquals("Wood", product.getProductType(),
+                "Product type should match requested type");
+        assertEquals(new BigDecimal("5.15"), product.getCostPerSquareFoot(),
+                "Product cost per sq ft should match");
+        assertEquals(new BigDecimal("4.75"), product.getLaborCostPerSquareFoot(),
+                "Product labor cost per sq ft should match");
     }
 
     @Test
-    void importFromFile() {
+    void testImportFromFileGetProduct() {
         assertDoesNotThrow(() -> testDao.importFromFile(),
                 "Importing from file should not throw an exception");
 
         // Verify some data was imported
         assertFalse(testDao.getAllProducts().isEmpty(), "Product list should not be empty after import");
+        assertEquals("Wood", testDao.getProduct("Wood").getProductType()
+                , "It should contain the wood product type");
+        assertEquals("Tile", testDao.getProduct("Tile").getProductType()
+                , "It should contain the tile product type");
+        assertEquals("Laminate", testDao.getProduct("Laminate").getProductType()
+                , "It should contain the laminate product type");
+        assertEquals("Carpet", testDao.getProduct("Carpet").getProductType()
+                , "It should contain the carpet product type");
         assertEquals(4, testDao.getAllProducts().size(), "There should be 4 types");
-
     }
+
+
+
 
 }
--- src/test/java/com/sg/floormastery/dao/TaxDAOFileImplTest.java
+++ src/test/java/com/sg/floormastery/dao/TaxDAOFileImplTest.java
@@ -17,14 +17,19 @@ class TaxDAOFileImplTest {
     }
 
     @Test
-    void getTax() {
+    void testGetTax() {
         BigDecimal taxRate = testDao.getTax("TX").getTaxRate();
         assertNotNull(taxRate, "Tax rate should not be null for a valid state");
-        assertTrue(taxRate.compareTo(BigDecimal.ZERO) > 0, "Tax rate should be greater than zero");
+        assertEquals( "TX", testDao.getTax("TX").getStateAbbreviation() ,
+                "Should have the Texas abbreviation");
+        assertEquals( "Texas", testDao.getTax("TX").getStateName() ,
+                "Should have the Texas state name");
+        assertEquals( new BigDecimal("4.45"), testDao.getTax("TX").getTaxRate() ,
+                "Should have the tax rate");
     }
 
     @Test
-    void getStatesAbbreviation() {
+    void testGetStatesAbbreviation() {
         Set<String> states = testDao.getStatesAbbreviation();
         assertNotNull(states, "States set should not be null");
         assertFalse(states.isEmpty(), "States set should not be empty");
@@ -38,11 +43,20 @@ class TaxDAOFileImplTest {
     }
 
     @Test
-    void importFromFile() {
+    void testImportFromFileGetStatesAbbreviation() {
         assertDoesNotThrow(() -> testDao.importFromFile(), "Importing from file should not throw an exception");
 
         // Verify some data was imported
         assertFalse(testDao.getStatesAbbreviation().isEmpty(), "States should not be empty after import");
+        assertTrue(testDao.getStatesAbbreviation().contains("TX")
+                , "It should contain the Texas state");
+        assertTrue(testDao.getStatesAbbreviation().contains("WA")
+                , "It should contain the Washington state");
+        assertTrue(testDao.getStatesAbbreviation().contains("CA")
+                , "It should contain the California state");
+        assertTrue(testDao.getStatesAbbreviation().contains("KY")
+                , "It should contain the Kentucky state");
+        assertEquals(4, testDao.getStatesAbbreviation().size(), "There should be 4 types");
     }
 
 }
